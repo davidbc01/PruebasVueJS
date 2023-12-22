fe89e35bf0c36733ee7b175f6c6dcfc874b11298
David <dbartolc01@iescastelar.com>
2023-12-22 01:24:10 +0100

    Modifico la vista de pruebas

--- src/views/PruebasView.vue
+++ src/views/PruebasView.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="about">
       <h1 class="text-2xl italic text-green-500">Esta es la página de pruebas</h1>
+      <p class="text-lg text-sky-500 hover:italic hover:font-bold">Page by davidbc01</p>
     </div>
 </template>
